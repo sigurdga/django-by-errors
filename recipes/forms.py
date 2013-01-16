74fc59813459592dcf0adcfe410b23dc7736d041
Sigurd Gartmann <sigurdga@sigurdga.no>
2013-01-16 11:59:03 +0100

    clean start, use branches next

--- recipes/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm, HiddenInput
-from recipes.models import Ingredient
-
-class IngredientForm(ModelForm):
-
-    class Meta:
-        model = Ingredient
-        widgets = {'recipe': HiddenInput()}
